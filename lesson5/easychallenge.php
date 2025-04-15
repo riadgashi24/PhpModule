214d621b1659a2652114bf3287af5fc49683c05f
riadgashi24 <gashiriad09@gmail.com>
2025-04-15 16:19:05 +0200

    easychllg

---
+++ lesson5/easychallenge.php
@@ -0,0 +1,8 @@
+<?php
+    $employees = array(
+        array("Name"=>"John Doe", "Age"=>"43","Positon"=>"Diector"),
+        array("Name"=>"Behxhet Pacolli", "Age"=>"53","Positon"=>"Milioner"),
+        array("Name"=>"Albin Kurti", "Age"=>"45","Positon"=>"Kryeminister"),
+        array("Name"=>"Naim Abazi", "Age"=>"60","Positon"=>"Kengtar"),
+        )
+?>
