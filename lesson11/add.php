e955633918fac9d5ff7b04160cf24db1c048124c
riadgashi24 <gashiriad09@gmail.com>
2025-05-13 16:16:40 +0200

    lesson 11

--- lesson11/add.php
+++ lesson11/add.php
@@ -28,7 +28,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Document</title>
 </head>
 <body>
     <a href="dashboard.php">
---
+++ lesson11/dashboard.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <style>
+        table,td,th{
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        td,th{
+           
+             padding: 10px 20px;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        include_once("config.php");
+        $sql = "SELECT* FROM users";
+        $getUsers = $conn -> prepare($sql);
+        $getUsers ->   execute();
+
+        $users = $getUsers -> fetchAll();
+    
+    ?>
+
+    <br><br>
+
+    <table>
+        <thead>
+            <th>id</th>
+            <th>name</th>
+            <th>surname</th>
+            <th>email</th>
+            <th>delete</th>
+            <th>update</th>
+        </thead>
+
+        <tbody>
+            <?php
+                foreach($users as $user){
+            ?>
+            <tr>
+                <td>
+                    <?=$user['id']?>
+                </td>
+                <td>
+                    <?=$user['name']?>
+                </td>
+                <td>
+                    <?=$user['surname']?>
+                </td>
+                <td>
+                    <?=$user['email']?>
+                </td>
+                <td>
+                    <?= "<a href='delete.php?id=$user[id]'>Delete</a>"?>
+                </td>
+                <td>
+                    <?= "<a href='update.php?id=$user[id]'>Update</a>"?>
+                </td>
+            </tr>
+        <?php
+                }
+        ?>
+        </tbody>
+    </table>
+        <a href="add.php">Add</a>
+</body>
+</html>
