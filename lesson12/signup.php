2884a0d6cbd97f970520ed9e94f07febb9a76eeb
riadgashi24 <gashiriad09@gmail.com>
2025-05-15 16:04:52 +0200

    lesson 12

---
+++ lesson12/signup.php
@@ -0,0 +1,27 @@
+<?php include("header.php") ?>
+
+  <div class="signup">
+    <form action="register.php" method="post" class="signin">
+      <h1 class="h33 mb-3 font-weight-normal">Please sign up</h1>
+      <label for="inputEmail" class="sr-only">Name</label>
+      <input type="text" id="inputEmail" class="form-control" placeholder="name" name="name" required autofocus>
+
+      <label for="inputEmail" class="sr-only">Surname</label>
+      <input type="text" id="inputEmail" class="form-control" placeholder="surname" name="surname" required autofocus>
+
+      <label for="inputEmail" class="sr-only">Username</label>
+      <input type="text" id="inputEmail" class="form-control" placeholder="username" name="username" required autofocus>
+
+      <label for="inputEmail" class="sr-only">Email</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="email" name="email" required autofocus>
+
+      <label for="inputEmail" class="sr-only">Password</label>
+      <input type="password" id="inputEmail" class="form-control" placeholder="password" name="password" required autofocus>
+
+      <button type="submit" class="btn btn-lg btn-primary btn-block" name="submit ">Sign Up</button>
+    </form>
+  </div>
+  
+
+
+  <?php include("footer.php") ?>
