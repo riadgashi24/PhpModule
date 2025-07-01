eb54ff232edab38498adb96c8ec5ec8023cda372
riadgashi24 <gashiriad09@gmail.com>
2025-07-01 15:35:05 +0200

    Enhance property page with dynamic titles and descriptions
    Updated property-single.php to display the property's actual title and a short description or excerpt from the full description. Also updated breadcrumb navigation to reflect the property title and corrected the home link. Added two new property images to the assets directory.

--- EstateAgency/property-single.php
+++ EstateAgency/property-single.php
@@ -58,22 +58,30 @@ if (!$property) {
     <!-- Page Title -->
     <div class="page-title" data-aos="fade">
       <div class="heading">
-        <div class="container">
-          <div class="row d-flex justify-content-center text-center">
-            <div class="col-lg-8">
-              <h1>Property Single</h1>
-              <p class="mb-0">Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo odio sint voluptas consequatur ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione sint. Sit quaerat ipsum dolorem.</p>
-            </div>
-          </div>
+      <div class="container">
+        <div class="row d-flex justify-content-center text-center">
+        <div class="col-lg-8">
+          <h1><?php echo htmlspecialchars($property['title']); ?></h1>
+          <p class="mb-0">
+          <?php 
+            if (!empty($property['short_description'])) {
+            echo htmlspecialchars($property['short_description']);
+            } else {
+            echo mb_strimwidth(strip_tags($property['description']), 0, 180, '...');
+            }
+          ?>
+          </p>
+        </div>
         </div>
       </div>
+      </div>
       <nav class="breadcrumbs">
-        <div class="container">
-          <ol>
-            <li><a href="index.html">Home</a></li>
-            <li class="current">Property Single</li>
-          </ol>
-        </div>
+      <div class="container">
+        <ol>
+        <li><a href="index.php">Home</a></li>
+        <li class="current"><?php echo htmlspecialchars($property['title']); ?></li>
+        </ol>
+      </div>
       </nav>
     </div><!-- End Page Title -->
 
